b02bfc1a0a6a710f6547ae7761dceeee3f3414a3
Teamofab <95554205+Teamofab@users.noreply.github.com>
2022-12-09 10:54:14 +0530

    Delete start.sh

--- start.sh
+++
@@ -1,6 +0,0 @@
-echo "Cloning Repo...."
-git clone https://github.com/Teamofab/teamurldeadxbot /teamurldeadxbot
-cd /teamurldeadxbot
-pip3 install -r requirements.txt
-echo "Starting Bot...."
-python3 bot.py
